b13ec030365c97db1ef76927acebe38b82d0fc39
Wattanai Sathuphan <nine.stay27@gmail.com>
2019-04-06 13:16:49 +0700

    adduer:0

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import firebase_api
 
 app = Flask(__name__)
 CORS(app)
@@ -7,6 +8,17 @@ CORS(app)
 @app.route('/')
 def root():
     return "Working"
+
+@app.route('/adduser')
+def adduser():
+    try:
+        uid = request.args.get("uid")
+        username = request.args.get("uname")
+        print("got args")
+        firebase_api.addUser(uid, username)
+        return jsonify({"status": "done"})
+    except:
+        return jsonify({"status": "error"})
 
 if __name__ == "__main__":
     app.run(debug = False,host="0.0.0.0", port=5000)
