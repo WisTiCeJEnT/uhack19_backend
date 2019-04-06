1d80474a79e241ec3d325a4242ef25b28eea0733
Wattanai Sathuphan <nine.stay27@gmail.com>
2019-04-06 15:18:13 +0700

    POST pls

--- app.py
+++ app.py
@@ -21,10 +21,10 @@ def adduser():
     except:
         return jsonify({"status": "error"})
 
-@app.route('/userdata', methods = ['GET'])
+@app.route('/userdata', methods = ['POST'])
 def getUserData():
     try:
-        if request.method == 'GET':
+        if request.method == 'POST':
             uid = request.args.get("uid")
             return jsonify({"status": "ok", "userData": firebase_api.getUser(uid)})
     except:
