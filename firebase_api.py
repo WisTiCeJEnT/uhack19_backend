fff571291087ca186a2b557731a004f73118961c
Wattanai Sathuphan <nine.stay27@gmail.com>
2019-04-06 12:55:05 +0700

    first push

---
+++ firebase_api.py
@@ -0,0 +1,14 @@
+import pyrebase
+
+config = {
+  "apiKey": "AIzaSyA8gvXnhMyyPaBLgLm7_eHFfBQA0UrasVQ",
+  "authDomain": "projectId.firebaseapp.com",
+  "databaseURL": "https://uhack19-4c33e.firebaseio.com/",
+  "storageBucket": "projectId.appspot.com"
+}
+
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
+data = {"name": "Mortimer 'Morty' Smith"}
+db.child("/").push(data)
+
